afd790bb37b4899e16d797fe1fa19dad03e04410
technodict <joglekar.omkar@gmail.com>
2016-07-20 04:10:35 +0530

    added lower case and upper case functions

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
-name=input("what is your name ?")
-print(name)  #prints the name 
+fname=input("what is your name ?")
+#print(name)  #prints the name 
+lname=input("what is your surname?")
+print("Hello "+fname+" "+lname)
+print(fname.lower() +" "+ lname.lower())
+print(fname.upper() +" "+ lname.upper())
+
+
+
+
+
+
 
