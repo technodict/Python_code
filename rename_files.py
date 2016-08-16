0cba81e8a1b0c3b1c4737268ca34970a70b13879
technodict <joglekar.omkar@gmail.com>
2016-08-17 00:33:29 +0530

    code to list all files in a filesystem

---
+++ rename_files.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import os
+
+def rename_files():
+    # get file names form a folder
+    file_list= os.listdir(r'/home/omkar/Downloads/Python/prank/prank')
+    print(file_list)
+
+    #for each file rename filename
+rename_files()
+
